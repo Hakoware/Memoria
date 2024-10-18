0cf74b30fc26c5f2312aec62ad258ba6e968c619
Hakoware <112647766+Hakoware@users.noreply.github.com>
2024-10-17 23:04:40 -0300

    Update SceneChanger.cs

--- Assets/GaussianAssets/Scripts/SceneChanger.cs
+++ Assets/GaussianAssets/Scripts/SceneChanger.cs
@@ -5,24 +5,17 @@ using UnityEngine.UIElements;
 public class SceneChangerUIToolkit : MonoBehaviour
 {
     private Button sceneChangeButton;
-
-    // Asigna el nombre de la escena a la que quieres cambiar
+    
     public string sceneToLoad;
 
     void OnEnable()
     {
-        // Obtén el UIDocument del componente
+        
         var root = GetComponent<UIDocument>().rootVisualElement;
-
-        // Encuentra el botón por su nombre
         sceneChangeButton = root.Q<Button>("sceneChangeButton");
         sceneChangeButton.focusable = false;
-
-        // Añadir el evento para cuando se haga clic
         sceneChangeButton.clicked += () => ChangeScene();
     }
-
-    // Método que cambia de escena
     void ChangeScene()
     {
         SceneManager.LoadScene(sceneToLoad);
